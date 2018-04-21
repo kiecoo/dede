d073bd7d04d0b3dd096b5360449a96713da5c8ca
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-21 16:10:20 +0800

    Update get-github-data.js

--- get-github-data.js
+++ get-github-data.js
@@ -6,16 +6,16 @@ function getGithubData () {
 
     var data= [
     {
-        "repo": "nicco88/personal-portfolio",
-        "url": "https://api.github.com/repos/nicco88/personal-portfolio",
-        "username": "ninabreznik",
-        "time": "2018-04-01T17:50:29Z"
+        repo: "nicco88/personal-portfolio",
+        url: "https://api.github.com/repos/nicco88/personal-portfolio",
+        username: "ninabreznik",
+        time: "2018-04-01T17:50:29Z"
     },
     {
-        "repo": "freeCodeCamp/devdocs",
-        "url": "https://api.github.com/repos/freeCodeCamp/devdocs",
-        "username": "serapath",
-        "time": "2018-03-27T04:51:07Z"
+        repo: "freeCodeCamp/devdocs",
+        url: "https://api.github.com/repos/freeCodeCamp/devdocs",
+        username: "serapath",
+        time: "2018-03-27T04:51:07Z"
     }]
     
     
